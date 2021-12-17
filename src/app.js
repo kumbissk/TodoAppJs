3fdb2e16fbbf0d7a8c1e8769d7b95c8d7347f84d
kumbissk <ramataka81@gmail.com>
2021-12-17 17:08:37 +0100

    recuperation des element du dom

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTY1NzAzOCwiZXhwIjoxOTU1MjMzMDM4fQ.EzDm1wsIr8aC8gW7YEVWDGxXrIEZPV6R7ZyVvJ4d7Bc"
+
+const API_URL = "https://tbzrjnqxccgkgvarbwpr.supabase.co/rest/v1/idees"
+
+//RECUPERER LES ELEMENTS DU DOM
+const propositionElement = document.getElementById("propositions")
+const ideeForm = document.querySelector("form")
+const inputTitre = document.querySelector("input#titre")
+const inputSuggestion = document.querySelector("textarea#suggestion")
+
+
+
